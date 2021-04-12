456c29724ea8e43cfd65ae387bd5c4533e48dfb1
jag-mosquera <67288840+jag-mosquera@users.noreply.github.com>
2021-04-12 18:57:28 -0500

    epic page

--- js/registro3.js
+++
@@ -1,6 +0,0 @@
-function volverAlapagina(){
-let boton;
-
-boton=document.getElementById("boton")
-window.location.assign("file:///C:/Users/PC%20MASTER%20160/Documents/programaruvc/proyecto%20integrador/eventos/indexeventos.html");
-}
